436e97ebd3d398e62dcc68c80084dc7cccd84885
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2024-11-14 20:05:36 +0600

    login with btns done

--- src/components/SocialLogin.jsx
+++ src/components/SocialLogin.jsx
@@ -1,10 +1,18 @@
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+
 const SocialLogin = () => {
   return (
     <div>
-      <h2 className="font-semibold">Login With</h2>
-      <div className="">
-        <button className="btn"></button>
-        <button className="btn"></button>
+      <h2 className="font-semibold mb-2">Login With</h2>
+      <div className=" *:w-full space-y-2">
+        <button className="btn">
+          <FcGoogle className="text-xl" /> Login With Google
+        </button>
+        <button className="btn">
+          <FaGithub className="text-xl" />
+          Login With Github
+        </button>
       </div>
     </div>
   );
